58c21e4a76f828c5eb31d28e14c0d4cabb7bbc79
Daniel Eden <dan.eden@me.com>
2018-10-18 21:28:10 -0700

    Add comments to index.js

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const csv = require('csvtojson')
 const emojiCsv =  './emoji.csv'
 
+// Pick a random element from an array
 const sample = (array) => array[Math.floor(Math.random() * array.length)]
 
 csv().fromFile('./emoji.csv').then(emoji => {
   let [a, b] = [sample(emoji), sample(emoji)]
+
+  // Naive safeguard against picking the same element twice
   if(a === b) b = sample(emoji)
 
+  // TODO: Add real account ID
   const pollCardPayload = {
     account_id: 'TBD',
     duration_in_minutes: 24*60,
@@ -14,6 +18,7 @@ csv().fromFile('./emoji.csv').then(emoji => {
     second_choice: b.utf,
   }
 
+  // TODO: Get card URI from post response above
   const pollCardURI = 'TBD' //response.data.card_uri
 
   const tweetPayload = {
@@ -21,6 +26,7 @@ csv().fromFile('./emoji.csv').then(emoji => {
     card_uri: pollCardURI,
   }
 
+  // TODO: Remove console.log-ing
   console.log(pollCardPayload)
 
 })
